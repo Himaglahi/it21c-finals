f1a917d811896d6cc2d5bbe2cc6474e50d99321b
Sir Cliff <dev.clfm5t12@gmail.com>
2023-12-14 13:16:55 +0800

    Finals

--- src/finalsAssessment/Main.java
+++ src/finalsAssessment/Main.java
@@ -4,12 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 	   
-		School school_01 = new School();
-		school_01.displaySchoolName();
 		
-		Programs[] program_list = Programs.initializePrograms();
-		Programs.displayPrograms(program_list);
-		Programs.selectProgram(program_list);
 		
 	}
 }
